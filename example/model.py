b896c57200a41eb9382d0b75b8c107064aed138c
Luis <luisernestoperezbello@gmail.com>
2023-07-04 14:38:18 -0600

    yes

--- example/model.py
+++ example/model.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class product(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
+    
