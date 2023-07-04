6fa65a396e7a081522ca03ba1d7fffdea770d5ec
Luis <luisernestoperezbello@gmail.com>
2023-07-04 12:17:11 -0600

    firstpage

--- example/views.py
+++ example/views.py
@@ -1,16 +1,6 @@
 # example/views.py
-from datetime import datetime
-
-from django.http import HttpResponse
+from django.http import response
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>This part of code must be a new page</h1>
-        
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+
+    return response(request,'index.html')
